a03d730277903246b0369aedb82e544dce308296
SteveJobzniak <SteveJobzniak@users.noreply.github.com>
2016-12-18 20:43:55 +0100

    Navigator: Enhancements, fixes and macOS support
    - Fixed the totally broken file path detection. It wasn't resolving the
    current path when files were opened with relative paths (such as "mpv
    *.mkv" in a folder). So I rewrote everything to check for an absolute
    path, and if not absolute then we prepend mpv's current working dir.
    - Made the path detection resolve relative paths ("/foo/bar/../bar/"
    becomes "/foo/bar/").
    - Added final failsafe so that if the path detection came up with a
    folder that doesn't actually exist, we fall back to starting the
    navigation from mpv's current working dir instead.
    - Rewrote the "isfolder" check in a more efficient, cross-platform way
    so that it works on both Linux and macOS.
    - Rewrote the folder scanner. Instead of wastefully using the overkill
    "find" utility (along with non-compatible flags that don't work on
    macOS), we now use "basename" instead and simply pass it a glob of all
    files in the folder. It returns the basenames (filename without path)
    for all files. That utility exists on both Linux and macOS, which means
    that the script is now supported on macOS.
    - Added a new function which clears the OSD, removes keybindings, and
    restores the font size to normal. And made all "reset/close/cancel"
    functions and timers use that instead.
    - Fixed the broken "safe path" generator. It was replacing all sequences
    of all kinds of whitespace with a single "\ " (a backslash and regular
    space. That meant that it didn't support folders with non-space
    whitespaces or more than a single space in a row. The new code solves
    that by properly escaping every whitespace character one by one.
    - Added useless macOS system paths to the default ignore list.
    - Enhanced keybinding of the main key so that you can choose it in the
    "settings" and can even set it to "nil" to allow you to bind it via
    input.conf instead.
    - Made it possible to rebind the "favorites" key via "settings".
    - Simplified favorites configuration (there is no need to number keys).
    - Added setting to choose how many OSD items to display per screen.
    - Added automatic font size switching, so that you can use a smaller
    font to see lots of files while the navigator is open and then return to
    your normal OSD font size afterwards.
    - Replaced all tabs with two spaces, because oh my god stop mixing tabs
    in half the file and two spaces in the other half! ;-)

--- navigator.lua
+++ navigator.lua
@@ -1,30 +1,44 @@
+--
+-- Mpv Filenavigator
+-- Author: donmaiq
+-- Contributors: SteveJobzniak
+-- URL: https://github.com/donmaiq/mpv-filenavigator
+--
 local settings = {
-    defaultpath = "/", --fallback if no file is open
-    forcedefault = false, --force navigation to start from defaultpath instead of currently playing file
-    --favorites in format { [index, starting from 1], 'Path to directory, notice trailing /' }
-    favorites =  {
-        [1] = '/media/HDD2/music/music/',
-        [2] = '/media/HDD/users/anon/Downloads/',
-        [3] = '/home/anon/',
-    },
-    --ignore paths, value anything that returns true for if statement
-    --you can ignore children without ignoring the parent
-    ignorePath = {
-      ['/bin']='1',['/boot']='1',['/cdrom']='1',['/dev']='1',['/etc']='1',['/lib']='1',['/lib32']='1',['/lib64']='1',
-      ['/srv']='1',['/sys']='1',['/snap']='1',['/root']='1',['/sbin']='1',['/proc']='1',['/opt']='1',['/usr']='1',['/run']='1',
-    },
-    --ignore folders and files that match patterns, make sure you use ^and$ to catch the whole str, value '' specifically
-    --read about patterns at https://www.lua.org/pil/20.2.html or http://lua-users.org/wiki/PatternsTutorial
-    ignorePat = {
-      ['^initrd%..*$']='',  --hide folders starting with initrd.
-      ['^vmlinuz.*$']='',
-      ['^lost%+found$']='',
-      ['^.*%.log$']='', --ignore extension .log
-    },
+  defaultpath = "/", --fallback if no file is open
+  forcedefault = false, --force navigation to start from defaultpath instead of currently playing file
+  --favorites in format { 'Path to directory, notice trailing /' }
+  favorites =  {
+    '/media/HDD2/music/music/',
+    '/media/HDD/users/anon/Downloads/',
+    '/home/anon/',
+  },
+  --ignore paths, value anything that returns true for if statement
+  --you can ignore children without ignoring the parent
+  ignorePath = {
+    --general linux system paths (some are used by macOS too):
+    ['/bin']='1',['/boot']='1',['/cdrom']='1',['/dev']='1',['/etc']='1',['/lib']='1',['/lib32']='1',['/lib64']='1',
+    ['/srv']='1',['/sys']='1',['/snap']='1',['/root']='1',['/sbin']='1',['/proc']='1',['/opt']='1',['/usr']='1',['/run']='1',
+    --useless macOS system paths:
+    ['/cores']='1',['/installer.failurerequests']='1',['/net']='1',['/private']='1',['/tmp']='1',['/var']='1'
+  },
+  --ignore folders and files that match patterns, make sure you use ^and$ to catch the whole str, value '' specifically
+  --read about patterns at https://www.lua.org/pil/20.2.html or http://lua-users.org/wiki/PatternsTutorial
+  ignorePat = {
+    ['^initrd%..*$']='',  --hide folders starting with initrd.
+    ['^vmlinuz.*$']='',
+    ['^lost%+found$']='',
+    ['^.*%.log$']='', --ignore extension .log
+  },
 
-    dynamic_binds = true, --navigation keybinds override arrowkeys and enter when activating script, false means keys are always actíve
-    menu_timeout = true, 	--timeouts after osd_dur seconds, else will be toggled by keybind
-    osd_dur = 5,					--osd duration if timeout is set to true
+  navigator_mainkey = "f",     --the key to bring up navigator's menu (will be auto-bound by the script, but you can set this to nil here to use input.conf instead!)
+  navigator_menu_favkey = "g", --cannot be nil; this key will always be bound when the menu is open, and is the key you use to cycle your favorites list!
+  dynamic_binds = true,        --navigation keybinds override arrowkeys and enter when activating navigation menu, false means keys are always actíve
+  menu_timeout = true,         --menu timeouts and closes itself after osd_dur seconds, else will be toggled by keybind
+  osd_dur = 5,                 --osd duration before the navigator closes, if timeout is set to true
+  osd_items_per_screen = 10,   --how many menu items to show per screen
+  navigator_font_size = 40,    --the font size to use for the OSD while the navigator is open
+  normal_font_size = mp.get_property("osd-font-size") --the OSD font size to return to when the navigator closes (get the osd-font-size property for default)
 }
 
 function os.capture(cmd, raw)
@@ -44,32 +58,48 @@ function handler(arg)
   timer:kill()
   if not path then
     if mp.get_property('path') and not settings.forcedefault then
-      path = string.sub(mp.get_property("path"), 1, string.len(mp.get_property("path"))-string.len(mp.get_property("filename")))
+      --determine path from currently playing file...
+      local workingdir = mp.get_property("working-directory")
+      local playfilename = mp.get_property("filename") --just the filename, without path
+      local playpath = mp.get_property("path") --can be relative or absolute depending on what args mpv was given
+      local firstchar = string.sub(playpath, 1, 1)
+      --first we need to remove the filename (may give us empty path if mpv was started in same dir as file)
+      path = string.sub(playpath, 1, string.len(playpath)-string.len(playfilename))
+      if (firstchar ~= "/") then --the path of the playing file wasn't absolute, so we need to add mpv's working dir to it
+        path = workingdir.."/"..path
+      end
+      --now resolve that path (to resolve things like "/home/anon/Movies/../Movies/foo.mkv")
+      path = resolvedir(path)
+      --lastly, check if the folder exists, and if not then fall back to the current mpv working dir
+      if (not isfolder(path)) then
+        path = workingdir
+      end
     else path = settings.defaultpath end
     dir,length = scandirectory(path)
   end
   local output = path.."\n\n"
-  local b = cursor - 5
+  local b = cursor - math.floor(settings.osd_items_per_screen / 2)
   if b > 0 then output=output.."...\n" end
   if b<0 then b=0 end
-  for a=b,b+10,1 do
+  for a=b,(b+settings.osd_items_per_screen),1 do
     if a==length then break end
     if a == cursor then
       output = output.."> "..dir[a].." <"
       if arg == "added" then output = output.." + added to playlist\n"
-      	elseif arg == "removed" then output = output.." - removed previous addition\n" else output=output.."\n" end
+      elseif arg == "removed" then output = output.." - removed previous addition\n" else output=output.."\n" end
     else
       output = output..dir[a].."\n"
     end
-    if a == b+10 then
+    if a == (b+settings.osd_items_per_screen) then
       output=output.."..."
     end
   end
+  mp.set_property("osd-font-size", settings.navigator_font_size)
   if not settings.menu_timeout then
-  	mp.osd_message(output, 100000)
+    mp.osd_message(output, 100000)
   else
-  	mp.osd_message(output, settings.osd_dur)
-  	timer:resume()
+    mp.osd_message(output, settings.osd_dur)
+    timer:resume()
   end
 end
 
@@ -95,8 +125,7 @@ end
 function childdir()
   local item = dir[cursor]
   if item then
-    local isfolder = os.capture('if test -d '..string.gsub(path..item, "%s+", "\\ ")..'; then echo "true"; fi')
-    if isfolder=="true" then
+    if isfolder(path..item) then
       changepath(path..dir[cursor].."/")
     else
       mp.commandv("loadfile", path..item, "append-play")
@@ -107,16 +136,23 @@ end
 
 --undo playlist file append
 function undo()
-	mp.commandv("playlist-remove", tonumber(mp.get_property('playlist-count'))-1)
-	handler("removed")
+  mp.commandv("playlist-remove", tonumber(mp.get_property('playlist-count'))-1)
+  handler("removed")
+end
+
+--close OSD and restore regular font size, and remove bindings
+function clearosd()
+  mp.osd_message("", 0.2)
+  mp.set_property("osd-font-size", settings.normal_font_size)
+  remove_keybinds()
 end
 
 --replace current playlist with directory or file
+--if directory, mpv will recursively queue all items found in the directory and its subfolders
 function opendir()
   local item = dir[cursor]
   if item then
-  	mp.osd_message("", 0.2)
-    remove_keybinds()
+    clearosd()
     mp.commandv("loadfile", path..item, "replace")
   end
 end
@@ -131,31 +167,49 @@ end
 
 --move up to the parent directory
 function parentdir()
-  local parent = os.capture('cd '..string.gsub(path, "%s+", "\\ ")..'; cd .. ; pwd').."/"
-  if parent == "//" then parent = "/" end
+  local parent = os.capture('cd '..string.gsub(path, "(%s)", "\\%1")..'; cd .. ; pwd').."/"
+  if (string.sub(parent, -2) == "//") then
+    parent = string.sub(parent, 1, -2) --negative 2 removes the last character
+  end
   changepath(parent)
+end
+
+--resolves relative paths such as "/home/foo/../foo/Music" (to "/home/foo/Music") if the folder exists!
+function resolvedir(dir)
+  safedir = string.gsub(dir, "(%s)", "\\%1")
+  local resolved = os.capture('test -d '..safedir..' && cd '..safedir..' && pwd').."/"
+  if (string.sub(resolved, -2) == "//") then
+    resolved = string.sub(resolved, 1, -2) --negative 2 removes the last character
+  end
+  return resolved
+end
+
+--true if path exists and is a folder, otherwise false
+function isfolder(dir)
+  return os.execute('test -d '..string.gsub(dir, "(%s)", "\\%1"))
 end
 
 function scandirectory(arg)
   local directory = {}
-  local search = string.gsub(arg, "%s+", "\\ ")..'*'
-  
+  local search = string.gsub(arg, "(%s)", "\\%1")
+
   local popen=nil
   local i = 0
-  popen = io.popen('find '..search..' -maxdepth 0 -printf "%f\\n" 2>/dev/null')
+  --get basenames by globbing all files, but avoid glob asterisk if dir was empty
+  popen = io.popen('basename '..search..'/* | sed "s/^\\*$//"')
   if popen then
-      for dirx in popen:lines() do
-          local matched = false
-          for match, replace in pairs(settings.ignorePat) do
-            if dirx:gsub(match, replace) == '' then matched = true end
-          end
-          if not settings.ignorePath[path..dirx] and not matched then
-            directory[i] = dirx
-            i=i+1
-          end
+    for dirx in popen:lines() do
+      local matched = false
+      for match, replace in pairs(settings.ignorePat) do
+        if dirx:gsub(match, replace) == '' then matched = true end
       end
+      if not settings.ignorePath[path..dirx] and not matched then
+        directory[i] = dirx
+        i=i+1
+      end
+    end
   else
-      print("error: could not scan for files")
+    print("error: could not scan for files")
   end
   return directory, i
 end
@@ -180,13 +234,13 @@ function cyclefavorite()
 end
 
 function add_keybinds()
-	mp.add_forced_key_binding("DOWN", "nav-down", navdown, "repeatable")
-	mp.add_forced_key_binding("UP", "nav-up", navup, "repeatable")
-	mp.add_forced_key_binding("ENTER", "nav-open", opendir)
-	mp.add_forced_key_binding("BS", "nav-undo", undo)
-	mp.add_forced_key_binding("RIGHT", "nav-forward", childdir)
-	mp.add_forced_key_binding("LEFT", "nav-back", parentdir)
-	mp.add_forced_key_binding("g", "nav-favorites", cyclefavorite)
+  mp.add_forced_key_binding("DOWN", "nav-down", navdown, "repeatable")
+  mp.add_forced_key_binding("UP", "nav-up", navup, "repeatable")
+  mp.add_forced_key_binding("ENTER", "nav-open", opendir)
+  mp.add_forced_key_binding("BS", "nav-undo", undo)
+  mp.add_forced_key_binding("RIGHT", "nav-forward", childdir)
+  mp.add_forced_key_binding("LEFT", "nav-back", parentdir)
+  mp.add_forced_key_binding(settings.navigator_menu_favkey, "nav-favorites", cyclefavorite)
 end
 
 function remove_keybinds()
@@ -197,10 +251,10 @@ function remove_keybinds()
     mp.remove_key_binding('nav-undo')
     mp.remove_key_binding('nav-forward')
     mp.remove_key_binding('nav-back')
-		mp.remove_key_binding("nav-favorites")
+    mp.remove_key_binding("nav-favorites")
   end
 end
-timer = mp.add_periodic_timer(settings.osd_dur, remove_keybinds)
+timer = mp.add_periodic_timer(settings.osd_dur, clearosd)
 timer:kill()
 if not settings.dynamic_binds then
   add_keybinds()
@@ -208,18 +262,24 @@ end
 
 active=false
 function activate()
-	if settings.menu_timeout then
-		handler()
-	else
-		if active then
-			mp.osd_message("")
-			remove_keybinds()
-			active=false
-		else
-			handler()
-			active=true
-		end
-	end
+  if settings.menu_timeout then
+    handler()
+  else
+    if active then
+      clearosd()
+      active=false
+    else
+      handler()
+      active=true
+    end
+  end
 end
 
-mp.add_key_binding("f", "navigator", activate)
+if (settings.navigator_mainkey ~= nil) then
+  --override defaults and input.conf
+  mp.add_forced_key_binding(settings.navigator_mainkey, "navigator", activate)
+else
+  --just register the binding but no key, so that the user can bind it themselves
+  --via input.conf, as follows: Alt+x script-binding navigator
+  mp.add_key_binding(nil, "navigator", activate)
+end
